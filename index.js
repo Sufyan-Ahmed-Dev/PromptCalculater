81dbd6220a52264bc2a4daf9699ee7edf2e994fc
Sufyan Ahmed <sufiyan03313074760>
2024-03-14 09:10:14 +0500

    alert changes

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let num1 = +prompt("Enter  Number1")
 let num2 = +prompt("Enter  Number2")
 
 alert(`Addition of both number ${num1 + num2}`)
-alert(`Addition of both number ${num1 - num2}`)
+alert(`Subtraction of both number ${num1 - num2}`)
 
-alert(`Addition of both number ${num1 * num2}`)
+alert(`Multiplication of both number ${num1 * num2}`)
 
-alert(`Addition of both number ${num1 / num2}`)
+alert(`Division of both number ${num1 / num2}`)
 
 
 
