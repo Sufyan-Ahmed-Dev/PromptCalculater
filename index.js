5f1eb7d841fc35a51737c5d42da29a037241aa2e
Sufyan Ahmed <sufiyan03313074760>
2024-03-14 09:07:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let num1 = +prompt("Enter  Number1")
+let num2 = +prompt("Enter  Number2")
+
+alert(`Addition of both number ${num1 + num2}`)
+alert(`Addition of both number ${num1 - num2}`)
+
+alert(`Addition of both number ${num1 * num2}`)
+
+alert(`Addition of both number ${num1 / num2}`)
+
+
+
+// string is always true 
+
+// if ("shahzad" < "khan"){
+//     console.log("learn")
+// }
+// else{
+//     console.log("something else")
+// }
